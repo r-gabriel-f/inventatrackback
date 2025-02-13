10a79b4fb89f7dd7e6f04caaab7794866f1aa315
robert <robert.flores@shipedge.com>
2025-02-12 21:57:04 -0400

    supabase2

--- src/db.js
+++ src/db.js
@@ -1,9 +1,18 @@
+const { Pool } = require('pg');
 
-const createClient = require('@supabase/supabase-js').createClient;
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,         
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD, 
+  port: process.env.PORTS,           
+});
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error al conectar a la base de datos:', err.stack);
+  }
+  console.log('Conexión exitosa a PostgreSQL');
+  release();
+});
 
-
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-const db = createClient(supabaseUrl, supabaseKey)
-
-module.exports = db;
+module.exports = pool;
