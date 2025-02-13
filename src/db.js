6ebc762547a52407bc6f0d36bce85dd6368228b3
robert <robert.flores@shipedge.com>
2025-02-12 21:31:02 -0400

    supabase data

--- src/db.js
+++ src/db.js
@@ -1,16 +1,9 @@
 
-const { Client } = require('pg');
-const client = new Client({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,        
-  user: process.env.DB_USER || 'robert', 
-  password: process.env.DB_PASSWORD || '10475442',
-  database: process.env.DB_NAME || 'inventorymaterial'
-});
+const createClient = require('@supabase/supabase-js').createClient;
 
-// Conectar a la base de datos
-client.connect()
-  .then(() => console.log('Conexión exitosa a PostgreSQL'))
-  .catch(err => console.error('Error de conexión a PostgreSQL', err.stack));
 
-module.exports = client;
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+const db = createClient(supabaseUrl, supabaseKey)
+
+module.exports = db;
