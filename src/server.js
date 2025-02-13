4856a3e1268d70fd51aea47a7412227a437de5f6
robert <robert.flores@shipedge.com>
2025-02-12 22:03:03 -0400

    update data

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use('/api', exitRoutes);
 app.use('/api', reportRoutes);
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORTS || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
